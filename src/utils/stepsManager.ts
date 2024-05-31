238cdb657222226380b5706d9ee455dee328cc70
rricol <rricol@proton.me>
2024-05-31 17:57:49 +0200

    handle more than one button for next-step

--- src/utils/stepsManager.ts
+++ src/utils/stepsManager.ts
@@ -12,34 +12,38 @@ interface Product {
 export function initSteps(): void {
   const step1Div = document.querySelector('[data-step="1"]') as HTMLElement;
   const step2Div = document.querySelector('[data-step="2"]') as HTMLElement;
-  const nextStepButton = document.querySelector('#next-to-step-2') as HTMLButtonElement;
+  const nextStepButtons = document.querySelectorAll(
+    '[data-action="next-step"]'
+  ) as NodeListOf<HTMLButtonElement>;
   const previousStepButton = document.querySelector('#previous-to-step-1') as HTMLButtonElement;
   const textarea = document.querySelector('[data-text-area]') as HTMLTextAreaElement;
 
   step2Div.style.display = 'none';
-  if (nextStepButton) {
-    nextStepButton.addEventListener('click', () => {
-      if (step1Div && step2Div && !nextStepButton.hasAttribute('disabled')) {
-        step1Div.style.display = 'none';
-        step2Div.style.display = 'block';
+  if (nextStepButtons) {
+    nextStepButtons.forEach((button) => {
+      button.addEventListener('click', () => {
+        if (step1Div && step2Div && !button.hasAttribute('disabled')) {
+          step1Div.style.display = 'none';
+          step2Div.style.display = 'block';
 
-        const products: Product[] = getCookie('selectedProducts') || [];
-        let emailContent = `Prix : ${getPricing()} CHF\n+9 CHF de frais de port\n\nProduits sélectionnés:\n`;
-        emailContent += products
-          .map((product) => {
-            const { type, title, quantity, quantityA3, quantityA2 } = product;
-            if (type === 'Infographie') {
-              return `${type}: ${title} - A3: ${quantityA3} - A2: ${quantityA2}`;
-            }
-            return `${type}: ${title} - Quantité: ${quantity}`;
-          })
-          .join('\n');
+          const products: Product[] = getCookie('selectedProducts') || [];
+          let emailContent = `Prix : ${getPricing()} CHF\n+9 CHF de frais de port\n\nProduits sélectionnés:\n`;
+          emailContent += products
+            .map((product) => {
+              const { type, title, quantity, quantityA3, quantityA2 } = product;
+              if (type === 'Infographie') {
+                return `${type}: ${title} - A3: ${quantityA3} - A2: ${quantityA2}`;
+              }
+              return `${type}: ${title} - Quantité: ${quantity}`;
+            })
+            .join('\n');
 
-        if (textarea) {
-          textarea.value = emailContent;
-          textarea.disabled = true;
+          if (textarea) {
+            textarea.value = emailContent;
+            textarea.disabled = true;
+          }
         }
-      }
+      });
     });
   }
 
