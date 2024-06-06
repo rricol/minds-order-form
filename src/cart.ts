7d912a8efe263989703e2509c56207cf85651a82
rricol <rricol@proton.me>
2024-06-06 14:57:47 +0200

    feature: cart in menu - v3.1.0

--- src/cart.ts
+++ src/cart.ts
@@ -1,7 +1,12 @@
+import { getResourceCount } from '$utils/getFunctions';
 import { handleAddRessourceToCart } from '$utils/handlersFunctions';
 
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('[data-nmra-action="add-to-cart"]').forEach((element) => {
     element.addEventListener('click', handleAddRessourceToCart);
+  });
+  document.querySelectorAll('[data-nmra-element="cart"]').forEach((element) => {
+    const count = element.querySelector('[data-nmra-element="cart-count"]');
+    if (count) count.textContent = getResourceCount().toString();
   });
 });
